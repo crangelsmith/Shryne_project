07e0368e8f8c81726a28578bf66ac47f117a45b7
crangelsmith <camila.rangel.smith@gmail.com>
2016-08-09 14:09:26 +0100

    testing connector hypothesis

--- Shryne/mining/connector.py
+++ Shryne/mining/connector.py
@@ -21,7 +21,7 @@ class ConnectDB(object):
         Creates a connection to a predifined Postgresql database
         '''
         self.server = SSHTunnelForwarder((self.remote_host, self.remote_port),
-                                    ssh_private_key=self.path_to_private_key,
+                                  #  ssh_private_key=self.path_to_private_key,
                                     ssh_username=self.user_name,
                                     local_bind_address=self.local_bind_address,
                                     remote_bind_address=self.remote_bind_address)
@@ -45,19 +45,20 @@ class ConnectDB(object):
         self.server.stop()
 
 
-# def main():
-#     '''
-#     Quick main script to test out whether or not
-#     we can connect to the database
-#     :return: None
-#     '''
-#
-#     dbconnection = ConnectDB("/home/sophie/.ssh/id_rsa.pub")
-#     conn = dbconnection.get_connection()
-#     c = conn.cursor()
-#     c.execute("SELECT * FROM feed_items LIMIT 1")
-#     result = [row for row in c.fetchall()]
-#     print(result)
-#     dbconnection.close_connection()
-#
-# main()
+def main():
+    '''
+    Quick main script to test out whether or not
+    we can connect to the database
+    :return: None
+    '''
+
+    dbconnection = ConnectDB("/Users/Camila/.ssh/ssh_key.pub")
+    conn = dbconnection.get_connection()
+    c = conn.cursor()
+    c.execute("SELECT * FROM feed_items LIMIT 1")
+    result = [row for row in c.fetchall()]
+    print(result)
+    dbconnection.close_connection()
+
+
+main()
