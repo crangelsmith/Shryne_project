d6b4528bd49b1954816f5672911bdbb864048dcd
dabillox <danielfisher0@gmail.com>
2016-08-10 15:06:24 +0100

    now assigning sentiment to text strings

--- Shryne/sentiment_analysis/vader_sentiment_analysis.py
+++ Shryne/sentiment_analysis/vader_sentiment_analysis.py
@@ -1,4 +1,5 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
 
 class SentimentAnalyser(object):
 
@@ -7,11 +8,24 @@ class SentimentAnalyser(object):
 
     def run_vader(self, df, col):
 
-        senti_result = self.ss(df[col])
-        df['positive'] = senti_result['pos']
-        df['negative'] = senti_result['pos']
-        df['neutral'] = senti_result['neu']
-        df['compound'] = senti_result['compound']
+        positive_list = []
+        negative_list = []
+        neutral_list = []
+        compound_list = []
+
+        for index, row in df.iterrows():
+            #TODO decide whether to remove string assigment following set up of df cleaning
+            senti_result = self.ss.polarity_scores(str(row[col]))
+
+            positive_list.append(senti_result['pos'])
+            negative_list.append(senti_result['neg'])
+            neutral_list.append(senti_result['neu'])
+            compound_list.append(senti_result['compound'])
+
+        df['postive'] = pd.Series(positive_list)
+        df['negative'] = pd.Series(negative_list)
+        df['neutral'] = pd.Series(neutral_list)
+        df['compound'] = pd.Series(compound_list)
 
         return df
 
@@ -22,11 +36,12 @@ if __name__ == "__main__":
     import Shryne.mining.connector as connector
     import Shryne.mining.query as query
 
-    q = "SELECT * FROM feed_items LIMIT 10"
+    q = "SELECT * FROM feed_items LIMIT 10;"
 
     dbconnection = connector.ConnectDB()
     conn = dbconnection.get_connection()
     the_query = query.Query(conn, q)
     query_df = the_query.get_query_dataframe()
     sentiment_analyser = SentimentAnalyser()
-    sentiment_analyser.run_vader(query_df, 'body')
+    result = sentiment_analyser.run_vader(query_df, 'body')
+    print result
