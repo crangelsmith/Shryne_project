c9d218516724c8422b64d425ad7ea90fbf4b1764
Sophie Cowie <sophie_cowie@hotmail.com>
2016-08-18 12:54:11 +0100

    Added a function to drop the rows with one-sided conversations

--- Shryne/analytics/feature_creation.py
+++ Shryne/analytics/feature_creation.py
@@ -1,3 +1,14 @@
+def drop_one_sided(df):
+    """Drop any rows which are part of a one sided conversation
+    i.e. messages only from a contact to a user or vice versa"""
+
+    group = df.groupby(['user_id', 'contact_id'])
+
+    new_df = group.filter(lambda x: len(x['to_from'].unique()) == 2)
+
+    return new_df
+
+
 
 def create_features(df):
 
