3c94fe4fb7bec11d529795fc97723190e0a7334c
Sophie Cowie <sophie_cowie@hotmail.com>
2016-08-19 08:42:58 +0100

    Merged email cleaning and dropping of one-sided conversations with emoji conversion

--- Shryne/cleaning/clean_df.py
+++ Shryne/cleaning/clean_df.py
@@ -43,8 +43,27 @@ def drop_one_sided(df):
 
     return new_df
 
-def clean_emails(df):
-    """Remove Forwarded parts of messages"""
+def clean_message(df):
+    """Takes a dataframe which wants to clean
+    a column labelled 'message'"""
+
+    # First remove any message columns with None or Nan as
+    df = df[df['message'].notnull()]
+
+    # Get rid of anything after these strings.
+    sep = ['\n--\n', 'Begin forwarded message', 'Forwarded message', '----------------------------------------']
+
+    for s in sep:
+        df['message'] = df['message'].apply(lambda x: x.split(s, 1)[0])
+
+    # Replace urls and other stuff
+    subs = ["\n", "On\s[A-Z][a-z]{2}\s[0-9]{1,3}[\s\S]*", r'https?:\/\/[\S]*[\s\n\r]+', r'www\.[\S]*[\s\n\r]+',
+            'Sent from my iPhone.*[\s\n\r]', r'Sent from my iPad.*[\s\n\r]', r'Sent from my Sony.*[\s\n\r]',
+            r'Sent from my Samsung.*[\s\n\r]']
+    for s in subs:
+        df['message'] = df['message'].str.replace(s, '', case=False, flags=re.MULTILINE)
+
+    return df
 
 
 def turn_emoji_to_emoticon(df, dictionary):
@@ -64,3 +83,22 @@ def remove_carriage_returns(df):
 
 def remove_tabs(df):
     return df['message'].str.replace('\t', ' ')
+
+    # First remove any message columns with None or Nan as
+    df = df[df['message'].notnull()]
+
+    # Get rid of anything after these strings.
+    sep = ['\n--\n', 'Begin forwarded message', 'Forwarded message', '----------------------------------------']
+
+    for s in sep:
+        df['message'] = df['message'].apply(lambda x: x.split(s, 1)[0])
+
+    # Replace urls and other stuff
+    subs = ["\n", "On\s[A-Z][a-z]{2}\s[0-9]{1,3}[\s\S]*", r'https?:\/\/[\S]*[\s\n\r]+', r'www\.[\S]*[\s\n\r]+',
+            'Sent from my iPhone.*[\s\n\r]', r'Sent from my iPad.*[\s\n\r]', r'Sent from my Sony.*[\s\n\r]',
+            r'Sent from my Samsung.*[\s\n\r]']
+    for s in subs:
+        df['message'] = df['message'].str.replace(s, '', case=False, flags=re.MULTILINE)
+
+    return df
+
