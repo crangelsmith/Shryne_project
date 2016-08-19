02e512be1190888ff52f330833d4278c2073c142
crangelsmith <camila.rangel.smith@gmail.com>
2016-08-19 17:34:03 +0100

    adding clustering investigations

--- Shryne/modeling/clustering.py
+++ Shryne/modeling/clustering.py
@@ -1,23 +1,36 @@
 from sklearn.cluster import KMeans
 import pandas as pd
+from scipy.cluster.vq import kmeans
+from scipy.spatial.distance import cdist,pdist
+from sklearn.decomposition import RandomizedPCA
+import numpy as np
+import scipy.stats as stats
+
 
 df = pd.read_pickle('../data/relationship_features_forclustering.pandas_df')
 
 df.dropna(inplace = True)
 
-df_new = df[['message_count', 'word_count','compound','response_time']]
+df_new = df[['message_count', 'word_count','compound','response_time','message_count_reciprocity']]
 
+pca = RandomizedPCA(n_components=2).fit(df_new) # pca is an instance of class randomizedPCA with data['data']
+
+X = pca.transform(df_new) # reduces data from (1797,64) to (1797,2) though not sure how.
+
+print(stats.describe(X))
 
 pred = []
 k_means = KMeans(n_clusters = 2, init='k-means++')
-pred = k_means.fit_predict(df_new)
+pred = k_means.fit_predict(X)
 inertia = k_means.inertia_
 cluster_centers = k_means.cluster_centers_
+
+df_new["prediction"] =pred
 
 print(pred.sum())
 print(len(pred))
 print(inertia)
-print(cluster_centers)
+# print(cluster_centers)
 
 
 
