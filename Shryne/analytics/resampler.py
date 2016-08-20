9e55a5e242b451b069198a7eb6eae00ca543360e
Alexander Green <j.alexander.green@googlemail.com>
2016-08-20 14:49:49 +0100

    added emoji count to resampler

--- Shryne/analytics/resampler.py
+++ Shryne/analytics/resampler.py
@@ -98,7 +98,8 @@ def resample_dataframe(df, period='D'):
     df.set_index(time_field, inplace=True, drop=False)
 
     # get the total word counts and for the user and contact
-    keys = ["word_count", "I_count", "You_count", "We_count", "Us_count", "Pet_count"]
+    keys = ["word_count", "I_count", "You_count", "We_count", "Us_count",
+            "Pet_count", "emoji_count"]
     for k in keys:
         output_df[k] = df[k].resample(period, how=_sum)
         output_df[k + "_user"] = df[~df["to_from"]][k].resample(period, how=_sum)
@@ -119,7 +120,8 @@ def resample_dataframe(df, period='D'):
     # now compute reciprocity between users
     output_df["sentiment_reciprocity"] = (output_df["compound_contact"] - output_df["compound_user"]).abs()
     for k in ["message_count", "word_count", "I_count", "You_count",
-              "We_count", "Us_count", "Pet_count", "response_time"]:
+              "We_count", "Us_count", "Pet_count", "response_time",
+              "emoji_count"]:
         output_df[k + "_reciprocity"] = find_ratio(output_df, k)
 
     # now normalise number of mesages, words, etc
@@ -127,6 +129,7 @@ def resample_dataframe(df, period='D'):
             "We_count_contact","Us_count_contact","I_count_user","You_count_user",
             "We_count_user","Us_count_user", "Us_count",
             "Pet_count_user","Pet_count_contact", "Pet_count",
+            "emoji_count_user","emoji_count_contact","emoji_count"
             "message_count","message_count_user","message_count_contact",
             "word_count_user","word_count_contact"]
     for k in keys:
