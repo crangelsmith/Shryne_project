0ed3b6fce492b59887f7c1d3d7b46fc1f998314c
crangelsmith <camila.rangel.smith@gmail.com>
2016-08-25 14:58:06 +0100

    adding build_model function

---
+++ Shryne/modeling/build_model.py
@@ -0,0 +1,18 @@
+import Shryne.config as config
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import cross_val_score
+
+#Build logistic regression model and run cross validation to check goodness of the model
+def build_model(df):
+
+    model = LogisticRegression()
+
+    X = df[config.predictors]
+    y = df["label"]
+    scores = cross_val_score(model, X, y, scoring='accuracy', cv=5)
+
+    if scores.mean()<config.robust_model.mean or scores.std()>config.robust_model.std:
+        print "Model is broken. Call a data scientist."
+
+    return model
+
