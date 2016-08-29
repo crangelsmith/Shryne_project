b370af76f3c851f08767d6eb2514e39120cb7349
dabillox <danielfisher0@gmail.com>
2016-08-29 10:25:48 +0100

    removed the limit from the database query

--- Shryne/config.py
+++ Shryne/config.py
@@ -3,7 +3,7 @@
 # set flag for processing type
 
 # Query for make_model.py
-q_make = "SELECT * FROM all_msgs_tf LIMIT 10000"
+q_make = "SELECT * FROM all_msgs_tf"
 
 # Query for run_model.py
 # Contact id must be changed to the contact you wish to analyse.
